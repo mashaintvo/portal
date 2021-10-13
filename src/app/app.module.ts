96e2ac721ff4e610b83d742f058740fcea5fefad
Maria Sanjuan <maria.sanjuan@ilumno.com>
2021-10-13 04:18:02 -0500

    estilos v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { ServicesComponent } from './services/services.component';
+import { ProductsComponent } from './products/products.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { PriceTableComponent } from './price-table/price-table.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { InfoBannerComponent } from './info-banner/info-banner.component';
+import { ContactComponent } from './contact/contact.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    ServicesComponent,
+    ProductsComponent,
+    ArticlesComponent,
+    PriceTableComponent,
+    TestimonialComponent,
+    InfoBannerComponent,
+    ContactComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
